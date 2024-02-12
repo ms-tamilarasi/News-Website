7fab2a0c54861b04ad3bc675a6a6329406dc8c63
Tamilarasi <tamil101520@gmail.com>
2024-02-12 11:25:19 +0530

    commit

--- src/components/common/footer/Footer.jsx
+++ src/components/common/footer/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
       <footer>
         <div className='container'>
           <div className='box logo'>
-            <img src='../images/tech-logo-footer.png' alt='' />
+            <img src='./images/tech-logo-footer.png' alt='' />
             <p>Busan is an amazing magazine Blogger theme that is easy to customize for your needs</p>
             <i className='fa fa-envelope'></i>
             <span> hello@beautiful.com </span> <br />
@@ -17,22 +17,22 @@ const Footer = () => {
           <div className='box'>
             <h3>SPORT</h3>
             <div className='item'>
-              <img src='../images/hero/hero1.jpg' alt='' />
+              <img src='./images/hero/hero1.jpg' alt='' />
               <p>Google To Boost Android Security In Few Days</p>
             </div>
             <div className='item'>
-              <img src='../images/hero/hero2.jpg' alt='' />
+              <img src='./images/hero/hero2.jpg' alt='' />
               <p>Cespedes play the winning Baseball Game</p>
             </div>
           </div>
           <div className='box'>
             <h3>CRICKET</h3>
             <div className='item'>
-              <img src='../images/hero/hero3.jpg' alt='' />
+              <img src='./images/hero/hero3.jpg' alt='' />
               <p>US Promises to give Intel aid to locate the soldiers</p>
             </div>
             <div className='item'>
-              <img src='../images/hero/hero1.jpg' alt='' />
+              <img src='./images/hero/hero1.jpg' alt='' />
               <p>Renewable energy dead as industry waits for Policy</p>
             </div>
           </div>
@@ -60,7 +60,7 @@ const Footer = () => {
         <div className='container flexSB'>
           <p>© All Rights Reserved</p>
           <p>
-            made with <i className='fa fa-heart'></i> By Tamilarasi
+            Made with <i className='fa fa-heart'></i> By Tamilarasi
           </p>
         </div>
       </div>
